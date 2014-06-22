b90c52ec2308dc35aa60a4939e617a5dff2977fb
impossiblity <buluttekelioglu@gmail.com>
2014-06-22 21:27:28 +0300

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,11 +1,15 @@
-function GameManager(size, AI, Actuator, StorageManager) {
+function GameManager(size, AI, InputManager, Actuator, StorageManager) {
   this.events= {};
   this.size           = size; // Size of the grid
+  this.inputManager   = new InputManager;
   this.ai   = new AI(thisbi);
   this.storageManager = new StorageManager;
   this.actuator       = new Actuator;
 
   this.startTiles     = 2;
+  
+  this.inputManager.on("restart", this.restart.bind(this));
+  this.inputManager.on("keepPlaying", this.keepPlaying.bind(this));
 
   this.ai.on("move", this.move.bind(this));
   
